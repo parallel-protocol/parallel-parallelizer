54d8972922b2db8bc87b616286a269345adafa81
FabienCoutant <your.primary.email@example.com>
2025-09-18 12:42:28 +0300

    refactor: assign setRate to keeper role

--- scripts/SetSavingRoles.s.sol
+++ scripts/SetSavingRoles.s.sol
@@ -11,11 +11,14 @@ contract SetSavingRoles is BaseScript {
   address saving = 0x9B3a8f7CEC208e247d97dEE13313690977e24459;
 
   function run() public broadcast {
-    bytes4[] memory guardianSelectors = new bytes4[](3);
+    bytes4[] memory guardianSelectors = new bytes4[](2);
     guardianSelectors[0] = Savings.togglePause.selector;
     guardianSelectors[1] = Savings.toggleTrusted.selector;
-    guardianSelectors[2] = Savings.setRate.selector;
     accessManager.setTargetFunctionRole(saving, guardianSelectors, Roles.GUARDIAN_ROLE);
+
+    bytes4[] memory keeperSelectors = new bytes4[](1);
+    keeperSelectors[0] = Savings.setRate.selector;
+    accessManager.setTargetFunctionRole(saving, keeperSelectors, Roles.KEEPER_ROLE);
 
     bytes4[] memory governorSelectors = new bytes4[](3);
     governorSelectors[0] = SavingsNameable.setNameAndSymbol.selector;
