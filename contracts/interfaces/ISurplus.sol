55ee60e50a6f681b163e8a9acd051b69814ede24
FabienCoutant <your.primary.email@example.com>
2026-01-08 17:11:50 +0200

    feat: add ISurplus interface and integrate into IParallelizer

---
+++ contracts/interfaces/ISurplus.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.28;
+
+/// @title ISurplus
+/// @author Cooper Labs
+/// @custom:contact security@cooperlabs.xyz
+interface ISurplus {
+  /// @dev This function will swap the surplus of a collateral for the stable asset link to the protocol.
+  /// @param collateral The collateral address to process the surplus of.
+  /// @return collateralSurplus The surplus of the collateral.
+  /// @return stableSurplus The surplus amount in stable calculated from the collateral surplus.
+  /// @return issuedAmount The amount of newly issued stablecoins from the collateral swapped
+  function processSurplus(address collateral)
+    external
+    returns (uint256 collateralSurplus, uint256 stableSurplus, uint256 issuedAmount);
+
+  /// @notice Releases the income to the payees
+  /// @return payees The addresses of the payees
+  /// @return amounts The amounts released to the payees
+  function release() external returns (address[] memory payees, uint256[] memory amounts);
+
+  /// @notice Computes the surplus of a collateral.
+  /// @param collateral The collateral address to compute the surplus of.
+  /// @return collateralSurplus The collateral surplus amount.
+  /// @return stableSurplus The surplus in stable amount.
+  function getCollateralSurplus(address collateral)
+    external
+    view
+    returns (uint256 collateralSurplus, uint256 stableSurplus);
+}
